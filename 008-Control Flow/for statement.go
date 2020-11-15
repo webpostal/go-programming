9a0099430abbe9d147cafeced5ea6ae36ab5e170
Rafael Martinez <webpostal@gmail.com>
2020-11-15 11:22:58 -0400

    Update for statement.go

--- 008-Control Flow/for statement.go
+++ 008-Control Flow/for statement.go
@@ -7,35 +7,27 @@ func main() {
 	for x := 1; x <= 10; x++ {
 
 		fmt.Printf("%d\t", x)
-
 	}
 
 	fmt.Println("")
 
 	y := 1
-
 	for y <= 10 {
 
 		fmt.Printf("%d\t", y)
-
 		y++
-
 	}
 
 	fmt.Println("")
 
 	z := 1
-
 	for {
 
 		if z > 10 {
-
 			break
-
 		}
 
 		fmt.Printf("%d\t", z)
-
 		z++
 
 	}
