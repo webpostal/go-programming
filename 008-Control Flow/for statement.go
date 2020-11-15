5456adff4505444fc53d041d5b598da23b00d098
webpostal <webpostal@gmail.com>
2020-11-15 11:20:02 -0400

    For Statement

---
+++ 008-Control Flow/for statement.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for x := 1; x <= 10; x++ {
+
+		fmt.Printf("%d\t", x)
+
+	}
+
+	fmt.Println("")
+
+	y := 1
+
+	for y <= 10 {
+
+		fmt.Printf("%d\t", y)
+
+		y++
+
+	}
+
+	fmt.Println("")
+
+	z := 1
+
+	for {
+
+		if z > 10 {
+
+			break
+
+		}
+
+		fmt.Printf("%d\t", z)
+
+		z++
+
+	}
+
+	fmt.Println("\nDone.")
+
+}
